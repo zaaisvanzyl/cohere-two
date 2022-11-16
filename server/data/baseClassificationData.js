486420fc461d067a6a45bed5152c26d06139abb5
zaaisvanzyl <vanzylzaais@gmail.com>
2022-11-16 10:28:13 -0500

    add igCaption example

--- server/data/baseClassificationData.js
+++ server/data/baseClassificationData.js
@@ -1,11 +1,11 @@
 const baseClassificationData = [
-    {"text": "Write an instagram caption based on the following image description", "label": "IG Caption"},
+    {"text": "Write an instagram caption based on the following image description", "label": "igCaption"},
     {"text": "Summarize the following academic paper", "label": "Summarize 1"},
     {"text": "Write a story about a robot who falls in love with a human", "label": "Write a story"},
     {"text": "Summarize this paragraph:", "label": "Summarize 1"},
     {"text": "Summarize this dialogue: Cohere offers an API to add cutting-edge language processing to any system. Cohere trains massive language models and puts them behind a simple API. Moreover, through training, users can create massive models customized to their use case and trained on their data. This way, Cohere handles the complexities of collecting massive amounts of text data, the ever evolving neural network architectures, distributed training, and serving models around the clock.", "label": "Summarize 1"},
     {"text": "Write a story about a cat that chases a mouse", "label": "Write a story"},
-    {"text": "Write an instagram caption for a photo of a cat", "label": "IG Caption"}
+    {"text": "Write an instagram caption for a photo of a cat", "label": "igCaption"}
 
 ]
 
