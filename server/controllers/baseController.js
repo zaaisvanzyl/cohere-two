375d5be797ddd038e0d8eb8f3de75d6686dfc235
zaaisvanzyl <vanzylzaais@gmail.com>
2022-11-17 15:37:42 -0500

    prompt based on specific input

--- server/controllers/baseController.js
+++ server/controllers/baseController.js
@@ -6,19 +6,20 @@ const cohere = require('cohere-ai');
 cohere.init(process.env.COHERE_API_KEY);
 
 const baseController = async (req, res) => {
+    const inputs = [req.body.inputs]
     const response = await cohere.classify({
         model: 'large',
-        inputs: [req.body.inputs],
+        inputs: inputs,
         examples: baseClassificationData
       });
-
     const prediction = response.body.classifications[0].prediction
 
     var findPrompt = generativePrompts.find(item => item.name === prediction);
+    var prompt = findPrompt.value + inputs + findPrompt.ending
 
     const generateResponse = await cohere.generate({
         model: 'xlarge',
-        prompt: findPrompt.value,
+        prompt: prompt,
         max_tokens: 500,
         temperature: 0.5,
         k: 0,
